7fb878d4091c4239a4ef06adf17002d05f7ace9d
christian <cpchristiandelmonte@gmail.com>
2022-05-27 16:44:39 +0800

    added vue-material-design-icon

--- src/layout/MainFooter.vue
+++ src/layout/MainFooter.vue
@@ -28,17 +28,21 @@
             </a>
           </li>
         </ul>
-      </nav>
+      </nav> -->
       <div class="copyright">
-        &copy; {{ year }}, made with <md-icon>favorite</md-icon> by
-        <a href="https://www.creative-tim.com/" target="_blank">Creative Tim</a>
-        for a better web.
+        &copy; {{ year }},Freeboard made with <vuejs :size="30" />
       </div>
-    </div> -->
+    <!-- </div> -->
   </footer>
 </template>
 <script>
+import vuejs from 'vue-material-design-icons/Vuejs.vue'
+import 'vue-material-design-icons/styles.css'
+
 export default {
+  components: {
+    vuejs
+  },
   props: {
     backgroundColor: String,
     type: String
@@ -50,4 +54,9 @@ export default {
   }
 };
 </script>
-<style></style>
+<style lang="scss" scoped>
+  footer {
+    display: flex;
+    justify-content: center;
+  }
+</style>
