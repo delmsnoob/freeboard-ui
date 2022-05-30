88a380c1ae42e29329a1e31401453a6a79176f91
christian <cpchristiandelmonte@gmail.com>
2022-05-30 18:31:14 +0800

    added 404 page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+    <div class="section page-header" :style="headerStyle">
+      <div class="container">
+        <h2>404</h2>
+        <h3>Page Not Found</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound',
+
+  bodyClass: "login-page",
+
+  props: {
+    header: {
+      type: String,
+      default: require("@/assets/img/nature-3.jpg")
+    }
+  },
+
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.header})`
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
