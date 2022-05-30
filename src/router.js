766faf41397c78a3e7e65662f6eb1b31f981f24a
christian <cpchristiandelmonte@gmail.com>
2022-05-30 18:27:41 +0800

    added 404 page, register, and dashboard

--- src/router.js
+++ src/router.js
@@ -4,7 +4,9 @@ import VueRouter from "vue-router"
 import Dashboard from "./views/Dashboard"
 import Landing from "./views/Landing"
 import Login from "./views/Login"
+import Register from "./views/Register"
 import Profile from "./views/Profile"
+import PageNotFound from "./views/PageNotFound"
 import MainNavbar from "./layout/MainNavbar"
 import MainFooter from "./layout/MainFooter"
 
@@ -38,9 +40,36 @@ const routes = [
     }
   },
   {
+    path: "/register",
+    name: "register",
+    components: { default: Register, header: '', footer: MainFooter },
+    props: {
+      header: { colorOnScroll: 400 }
+    }
+  },
+  {
     path: "/profile",
     name: "profile",
     components: { default: Profile, header: MainNavbar, footer: MainFooter },
+    props: {
+      header: { colorOnScroll: 400 },
+      footer: { backgroundColor: "black" }
+    }
+  },
+  {
+    path: "/dashboard/:username",
+    name: "user-dashboard",
+    components: { default: Dashboard, header: MainNavbar, footer: MainFooter },
+    props: {
+      header: { colorOnScroll: 400 },
+      footer: { backgroundColor: "black" }
+    }
+  },
+  // 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'paeg-not-found',
+    components: { default: PageNotFound, header: MainNavbar, footer: MainFooter },
     props: {
       header: { colorOnScroll: 400 },
       footer: { backgroundColor: "black" }
